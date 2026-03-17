6a4f551257b2a5e20982be4b2f666d04fce2accf
jazzu72 <lesane1972@gmail.com>
2026-03-17 09:32:56 -0400

    Create decoy_network.py

---
+++ Kano/Kano/Kano/decoy_network.py
@@ -0,0 +1,8 @@
+"""
+Decoy Network
+Redirects attackers into controlled decoy environments.
+"""
+
+class DecoyNetwork:
+    def redirect(self, ip):
+        print(f"Redirecting {ip} to decoy environment.")
