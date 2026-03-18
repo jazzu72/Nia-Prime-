050f5c9a4d672d76a050bcf4be23b1d2fddef639
jazzu72 <lesane1972@gmail.com>
2026-03-18 09:05:36 -0400

    Update events _bus.js

--- events _bus.js
+++ events _bus.js
@@ -8,3 +8,14 @@ module.exports = {
     handlers.push(handler);
   }
 };
+const radarHandler = require("../handlers/radarHandler");
+const vaultInflowHandler = require("../handlers/vaultInflowHandler");
+const distributionHandler = require("../handlers/distributionHandler");
+const walletUpdateHandler = require("../handlers/walletUpdateHandler");
+const governanceHandler = require("../handlers/governanceHandler");
+
+handlers.push(radarHandler);
+handlers.push(vaultInflowHandler);
+handlers.push(distributionHandler);
+handlers.push(walletUpdateHandler);
+handlers.push(governanceHandler);
