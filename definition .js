311aef10b6c251ac072798c170dccfedc0b44460
jazzu72 <lesane1972@gmail.com>
2026-03-18 09:12:03 -0400

    Create definition .js

---
+++ definition .js
@@ -0,0 +1,9 @@
+module.exports = {
+  wallets: [
+    { name: "Founder", key: "founder" },
+    { name: "IRS", key: "irs" },
+    { name: "Bills", key: "bills" },
+    { name: "Growth", key: "growth" },
+    { name: "Opportunity", key: "opportunity" }
+  ]
+};
